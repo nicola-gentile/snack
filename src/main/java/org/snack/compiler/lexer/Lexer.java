d187dc53d98a76be8512594022a157670be1e123
nicola-gentile <nicola.gentile.developer@gmail.com>
2024-11-08 11:13:29 +0100

    Fix: fix of scanIntegerNumber() and scanRealNumber() methods Scanner default methods for reading BigDecimal and BigInteger do not work because the scanner would parse every character until a separator(like a space). The code "12.3)" does not get parsed correctly because of the ')' character. This commit fix this bug creating a custom reader using regular expression.

--- src/main/java/org/snack/compiler/lexer/Lexer.java
+++ src/main/java/org/snack/compiler/lexer/Lexer.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -14,20 +16,25 @@ public class Lexer {
     @NonNull
     Scanner scanner;
 
-    public static final String IDENTIFIER_REGEX = "\\$?[_a-zA-Z][_a-zA-Z_0-9'\\?]*!?|[+\\-*/<>|~^&]+";
-
+    public static final String IDENTIFIER_REGEX = "\\$?[_a-zA-Z][_a-zA-Z0-9'?]*!?|[+\\-*/<>|~^&]+";
+    public static final String REAL_NUM_REGEX = "(?:([0-9]+\\.[0-9]*)|(\\.[0-9]+))(?:[eE][0-9]+)?";
+    public static final String INTEGER_NUM_REGEX = "[0-9]+|0[xX][0-9A-Fa-f]+|0[bB][01]+|0[oO][0-7]+";
     @Getter(lazy=true)
-    final private static Pattern identifierPattern = Pattern.compile("^\\$?[_a-zA-Z][_a-zA-Z0-9'?]*!?");
+    final private static Pattern identifierPattern = Pattern.compile(IDENTIFIER_REGEX);
+    @Getter(lazy = true)
+    final private static Pattern realNumberPattern = Pattern.compile(REAL_NUM_REGEX);
+    @Getter(lazy = true)
+    final private static Pattern integerNumberPattern = Pattern.compile(INTEGER_NUM_REGEX);
 
     public Optional<IntegerNumber> scanIntegerNumber() {
-        return scanner.hasNextBigInteger() ?
-                Optional.of(new IntegerNumber(scanner.nextBigInteger())) :
+        return scanner.hasNext(getIntegerNumberPattern()) ?
+                Optional.of(new IntegerNumber(scanner.next(getIntegerNumberPattern()))) :
                 Optional.empty();
     }
 
     public Optional<RealNumber> scanRealNumber() {
-        return scanner.hasNextBigDecimal() ?
-                Optional.of(new RealNumber(scanner.nextBigDecimal())) :
+        return scanner.hasNext(getRealNumberPattern()) ?
+                Optional.of(new RealNumber(scanner.next(getRealNumberPattern()))) :
                 Optional.empty();
     }
 
