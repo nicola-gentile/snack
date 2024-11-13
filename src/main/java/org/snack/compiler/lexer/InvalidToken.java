6a590a91799ed7dfe7c9f528858b08b0e8b2f6d9
nicola-gentile <nicola.gentile.developer@gmail.com>
2024-11-13 23:31:10 +0100

    Update: InvalidToken.java now contains a direct method to associate an error code to an error message

--- src/main/java/org/snack/compiler/lexer/InvalidToken.java
+++ src/main/java/org/snack/compiler/lexer/InvalidToken.java
@@ -1,6 +1,20 @@
 package org.snack.compiler.lexer;
 
 import lombok.NonNull;
+import lombok.val;
 
-public record InvalidToken(@NonNull String text, @NonNull String errorMessage) implements Token {
+import java.util.Map;
+
+public record InvalidToken(@NonNull String text, @NonNull LexerErrorCode errorCode) implements Token {
+
+    final static private Map<LexerErrorCode, String> codeMessageMap = Map.of(
+            LexerErrorCode.INVALID_CHARACTER_TOKEN, "Invalid character syntax",
+            LexerErrorCode.INVALID_STRING_TOKEN, "Invalid string syntax",
+            LexerErrorCode.UNKNOWN_ESCAPE_CHARACTER, "Unknown escape character"
+    );
+
+    public String getErrorMessage() {
+        return codeMessageMap.get(errorCode());
+    }
+
 }
